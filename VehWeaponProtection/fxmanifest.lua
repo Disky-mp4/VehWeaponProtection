53c181e6631b6f845414564d825c6cf9c26e44b6
Disky <73770320+Disky-mp4@users.noreply.github.com>
2021-11-19 21:11:07 -0500

    added permissions

--- VehWeaponProtection/fxmanifest.lua
+++ VehWeaponProtection/fxmanifest.lua
@@ -2,8 +2,8 @@ fx_version 'cerulean'
 games { 'rdr3', 'gta5' }
 
 author 'Disky#9999' -- Discord
-description 'Simple script to delete a player\'s vehicle if it has weapons.'
-version '1.0.0' -- this (as a whole) most likely won't be updated.
+description 'Now with permissions, simple script to delete a player\'s vehicle if it has weapons.'
+version '1.0.1' -- this (as a whole) most likely won't be updated.
 
 client_script 'client.js'
 server_script 'server.js'
--- VehWeaponProtection/server.js
+++ VehWeaponProtection/server.js
@@ -2,3 +2,9 @@ onNet('server:logDeletion', () => {
     const playerName = GetPlayerName(source);
     console.log(`Weaponized vehicle has been deleted. Player info - Name: ${playerName} | Id: ${src}`); // logs to the server's console
 });
+
+onNet('disky.vehweapons', () => {
+    if(!IsPlayerAceAllowed(source, 'disky.weaponizedvehicless')){
+        emitNet('disky.vehweapons2', source);
+    }
+})
